80d0ca1e12b4f13e8f707236d2e29d9de53eb56b
MrKabado <jersonjaybonghanoy@gmail.com>
2025-08-06 23:27:42 +0800

    done services and projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Projects({title, Icon, description, image, alt, titleColor, iconColor, onClick}) {
   return (
-    <div className='w-full border p-3 rounded-xl bg-[#1E293B] my-3'>
-      <h1 className={`flex items-center text-lg font-semibold ${titleColor}`}>{title}<Icon className={`w-8 inline-block ${iconColor}`}/></h1>
+    <div className='w-full border p-3 rounded-xl bg-[#1E293B]'>
+      <h1 className={`flex items-center text-lg font-semibold ${titleColor}`}>{title}<Icon className={`w-7 inline-block ${iconColor}`}/></h1>
       <p className="text-sm text-white">{description}</p>
       <img className='border rounded-md mt-3' onClick={onClick} src={image} alt={alt} />
     </div>
