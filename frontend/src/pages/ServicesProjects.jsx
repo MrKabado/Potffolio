37b3178d0736d6bcc81ce846637bfa03f467ac64
MrKabado <jersonjaybonghanoy@gmail.com>
2025-09-12 11:34:13 +0800

    added some

--- frontend/src/pages/ServicesProjects.jsx
+++ frontend/src/pages/ServicesProjects.jsx
@@ -33,35 +33,50 @@ function ServicesProjects() {
     window.open('https://bonghanoy-jerson-jay-portfolio.netlify.app/', '_blank');
   }
 
+  const offerData = [
+    {
+      title: "Custom Website Design",
+      description: "I create websites that match your brand’s personality and goals, blending appealing visuals with functionality to keep visitors engaged.",
+      icon: <PaintBrushIcon className='w-8 md:w-9'/>,
+      style: "shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400 hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default",
+    },
+    {
+      title: "Responsive and Cross-Device Layouts",
+      description: "Your website will look great and work smoothly on desktops, tablets, and mobile devices, ensuring a consistent experience for all users.",
+      icon: <DevicePhoneMobileIcon className='w-8 md:w-9'/>,
+    },
+    {
+      title: "User-Centered Interface Design",
+      description: "I design layouts and navigation that make it easy for visitors to find what they need, improving overall usability and satisfaction.",
+      icon: <UserCircleIcon className='w-8 md:w-9'/>,
+    },
+    {
+      title: "Website Maintenance and Updates",
+      description: "I provide regular updates, content changes, and technical support to keep your website secure, up-to-date, and running smoothly as your business evolves.",
+      icon: <ArrowPathIcon className='w-8 md:w-9'/>,
+    }
+  ]
+
 
   return (
     <div className='p-5 text-[#1E293B] border-l-5 rounded-2xl font-poppins xl:px-10'>
       <h1 className='flex items-center text-xl font-semibold md:text-2xl'>What I Create For You</h1>
 
       <div className='grid grid-col gap-5 my-5 border-gray-400 border-b-4 pb-10 md:grid-cols-2'>
-        <div className='shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400 hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default'>
-          <PaintBrushIcon className='w-8 md:w-9'/>
-          <h1 className='font-semibold text-md md:text-lg'>Custom Website Design</h1>
-          <p className='text-sm md:text-base'>I create websites that match your brand’s personality and goals, blending appealing visuals with functionality to keep visitors engaged.</p>
-        </div>
+        {
+          offerData.map((data, index) => (
+            <div key={index} className='shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400 hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default'>
+              {data.icon}
+              <h1 className='font-semibold text-md md:text-lg'>
+                {data.title}
+              </h1>
 
-        <div className='shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400  hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default'>
-          <DevicePhoneMobileIcon className='w-8 md:w-9'/>
-          <h1 className='font-semibold text-md md:text-lg'>Responsive and Cross-Device Layouts</h1>
-          <p className='text-sm md:text-base'>Your website will look great and work smoothly on desktops, tablets, and mobile devices, ensuring a consistent experience for all users.</p>
-        </div>
-
-        <div className='shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400  hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default'>
-          <UserCircleIcon className='w-8 md:w-9'/>
-          <h1 className='font-semibold text-md md:text-lg'>User-Centered Interface Design</h1>
-          <p className='text-sm md:text-base'>I design layouts and navigation that make it easy for visitors to find what they need, improving overall usability and satisfaction.</p>
-        </div>
-
-        <div className='shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400  hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default'>
-          <ArrowPathIcon className='w-8 md:w-9'/>
-          <h1 className='font-semibold text-md md:text-lg'>Website Maintenance and Updates</h1>
-          <p className='text-sm md:text-base'>I provide regular updates, content changes, and technical support to keep your website secure, up-to-date, and running smoothly as your business evolves.</p>
-        </div>
+              <p className='text-sm md:text-base'>
+                {data.description}
+              </p>
+            </div>
+          ))
+        }    
       </div>
 
       <div className='my-10'>
