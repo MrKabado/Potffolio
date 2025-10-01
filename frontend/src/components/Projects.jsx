b5546bc41c548ae7c7143125936d34cb7a13e6ca
MrKabado <jersonjaybonghanoy@gmail.com>
2025-10-01 13:32:50 +0800

    add projects

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -1,24 +1,37 @@
-import React from 'react'
+import React from "react";
 
-function Projects({title, Icon, description, image, alt, titleColor, iconColor, onClick, imgClass}) {
+function Projects({
+  title,
+  Icon,
+  description,
+  image,
+  alt,
+  titleColor,
+  iconColor,
+  onClick,
+  imgClass,
+}) {
   return (
-    <div className='w-full border p-3 rounded-xl bg-[#1E293B] flex flex-col justify-between'>
+    <div className="w-full border p-3 rounded-xl bg-[#1E293B] flex flex-col justify-between">
       <span>
-        <h1 className={`flex items-center text-lg font-semibold ${titleColor} md:text-xl`}>
-          {title}<Icon className={`w-7 inline-block ${iconColor} md:w-9`}/>
+        <h1
+          className={`flex items-center text-lg font-semibold ${titleColor} md:text-xl`}
+        >
+          {title}
+          <Icon className={`w-7 inline-block ${iconColor} md:w-9`} />
         </h1>
 
-        <p className="text-sm text-white md:text-lg">{description}</p>
+        <p className="text-sm text-gray-300 md:text-lg">{description}</p>
       </span>
-      <img 
-        className={`border-2 border-transparent transition duration-200 rounded-md mt-3 hover:cursor-pointer active:cursor-wait ${imgClass} hover:border-white`} 
-        onClick={onClick} 
-        src={image} 
-        alt={alt} 
-        title='visit'
+      <img
+        className={`border-2 border-transparent transition duration-200 rounded-md mt-3 hover:cursor-pointer active:cursor-wait ${imgClass} hover:border-white`}
+        onClick={onClick}
+        src={image}
+        alt={alt}
+        title="visit"
       />
     </div>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
--- frontend/src/pages/ServicesProjects.jsx
+++ frontend/src/pages/ServicesProjects.jsx
@@ -1,141 +1,172 @@
-import React from 'react'
-import {PaintBrushIcon, UserCircleIcon, DevicePhoneMobileIcon, ArrowPathIcon} from '@heroicons/react/24/outline'
-import { Laugh, Dumbbell, ScrollText, GraduationCap } from "lucide-react";
+import React from "react";
+import {
+  PaintBrushIcon,
+  UserCircleIcon,
+  DevicePhoneMobileIcon,
+  ArrowPathIcon,
+} from "@heroicons/react/24/outline";
+import {
+  Laugh,
+  Dumbbell,
+  ScrollText,
+  GraduationCap,
+  ShoppingCart,
+} from "lucide-react";
 
-import Projects from '../components/Projects'
+import Projects from "../components/Projects";
 
-import Portfolio1 from '../assets/output-1.png'
-import FitXpert from '../assets/FitXpert.png'
-import RandomJokes from '../assets/output-2.png'
-import IntramsSurvey from '../assets/IntramsSurvey.png'
-import Portfolio2 from '../assets/Portfolio2.png'
-
+import Portfolio1 from "../assets/output-1.png";
+import FitXpert from "../assets/FitXpert.png";
+import RandomJokes from "../assets/output-2.png";
+import IntramsSurvey from "../assets/IntramsSurvey.png";
+import Portfolio2 from "../assets/Portfolio2.png";
+import RandomShii from "../assets/random-shii.png";
 
 function ServicesProjects() {
+  // For external links
+  const projectLinks = {
+    fitExpert: "https://fitxpert.netlify.app/",
+    intramsSurvey: "https://intrams-survey.netlify.app/",
+    portfolio1: "https://jersonjay-portfolio.netlify.app/",
+    randomJokes: "https://random-fun-jokes.netlify.app/",
+    portfolio2: "https://bonghanoy-jerson-jay-portfolio.netlify.app/",
+    randomshii: "https://random-shii.vercel.app/",
+  };
 
-  const fitExpert = () => {
-    window.open('https://fitxpert.netlify.app/', '_blank');
-  }
-
-  const intramsSurvey = () => {
-    window.open('https://intrams-survey.netlify.app/', '_blank');
-  }
-
-  const portfolio1 = () => {
-    window.open('https://jersonjay-portfolio.netlify.app/', '_blank');
-  }
-
-  const randomJokes = () => {
-    window.open('https://random-fun-jokes.netlify.app/', '_blank');
-  }
-
-  const portfolio2 = () => {
-    window.open('https://bonghanoy-jerson-jay-portfolio.netlify.app/', '_blank');
-  }
-
+  // Offer data
   const offerData = [
     {
       title: "Custom Website Design",
-      description: "I create websites that match your brand’s personality and goals, blending appealing visuals with functionality to keep visitors engaged.",
-      icon: <PaintBrushIcon className='w-8 md:w-9'/>,
-      style: "shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400 hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default",
+      description:
+        "I create websites that match your brand’s personality and goals, blending appealing visuals with functionality to keep visitors engaged.",
+      icon: <PaintBrushIcon className="w-8 md:w-9" />,
     },
     {
       title: "Responsive and Cross-Device Layouts",
-      description: "Your website will look great and work smoothly on desktops, tablets, and mobile devices, ensuring a consistent experience for all users.",
-      icon: <DevicePhoneMobileIcon className='w-8 md:w-9'/>,
+      description:
+        "Your website will look great and work smoothly on desktops, tablets, and mobile devices, ensuring a consistent experience for all users.",
+      icon: <DevicePhoneMobileIcon className="w-8 md:w-9" />,
     },
     {
       title: "User-Centered Interface Design",
-      description: "I design layouts and navigation that make it easy for visitors to find what they need, improving overall usability and satisfaction.",
-      icon: <UserCircleIcon className='w-8 md:w-9'/>,
+      description:
+        "I design layouts and navigation that make it easy for visitors to find what they need, improving overall usability and satisfaction.",
+      icon: <UserCircleIcon className="w-8 md:w-9" />,
     },
     {
       title: "Website Maintenance and Updates",
-      description: "I provide regular updates, content changes, and technical support to keep your website secure, up-to-date, and running smoothly as your business evolves.",
-      icon: <ArrowPathIcon className='w-8 md:w-9'/>,
-    }
-  ]
+      description:
+        "I provide regular updates, content changes, and technical support to keep your website secure, up-to-date, and running smoothly as your business evolves.",
+      icon: <ArrowPathIcon className="w-8 md:w-9" />,
+    },
+  ];
 
+  // Projects data
+  const projectsData = [
+    {
+      title: "My Portfolio",
+      Icon: UserCircleIcon,
+      description:
+        "A personal Portfolio made using basic HTML, CSS, and a bit of JavaScript.",
+      image: Portfolio1,
+      alt: "Portfolio",
+      titleColor: "text-blue-400",
+      link: projectLinks.portfolio1,
+    },
+    {
+      title: "Random Jokes",
+      Icon: Laugh,
+      description:
+        "A simple school output built with HTML, CSS, and JavaScript that tells random jokes.",
+      image: RandomJokes,
+      alt: "Random Jokes",
+      titleColor: "text-[#8DA4D0]",
+      link: projectLinks.randomJokes,
+    },
+    {
+      title: "FitXpert",
+      Icon: Dumbbell,
+      description:
+        "A basic school project created using HTML, CSS, and JavaScript. It's a gym website that displays pricing and allows users to apply for the program, and similar features.",
+      image: FitXpert,
+      alt: "FitXpert",
+      titleColor: "text-red-400",
+      link: projectLinks.fitExpert,
+    },
+    {
+      title: "Intrams Survey",
+      Icon: ScrollText,
+      description:
+        "A school project that collects student feedback on intramurals, including favorite events and suggestions, and displays the results in a simple interface.",
+      image: IntramsSurvey,
+      alt: "Intrams Survey",
+      titleColor: "text-sky-300",
+      link: projectLinks.intramsSurvey,
+    },
+    {
+      title: "My Portfolio 2",
+      Icon: GraduationCap,
+      description:
+        "My second personal portfolio, built with ReactJS and TailwindCSS. It showcases my improved design approach, though it is not yet fully responsive.",
+      image: Portfolio2,
+      alt: "Portfolio2",
+      titleColor: "text-white",
+      link: projectLinks.portfolio2,
+    },
+    {
+      title: "RandomShii",
+      Icon: ShoppingCart,
+      description:
+        "A simple project about ReactJS and API integration, manipulating API data; its functions are to map the products, add items to cart, place an order, categorize items, search, filter, and implement pagination.",
+      image: RandomShii,
+      alt: "RandomShii",
+      titleColor: "text-gray-200",
+      link: projectLinks.randomshii,
+    },
+  ];
 
   return (
-    <div className='p-5 text-[#1E293B] border-l-5 rounded-2xl font-poppins xl:px-10'>
-      <h1 className='flex items-center text-xl font-semibold md:text-2xl'>What I Create For You</h1>
+    <div className="p-5 text-[#1E293B] border-l-5 rounded-2xl font-poppins xl:px-10">
+      <h1 className="flex items-center text-xl font-semibold md:text-2xl">
+        What I Create For You
+      </h1>
 
-      <div className='grid grid-col gap-5 my-5 border-gray-400 border-b-4 pb-10 md:grid-cols-2'>
-        {
-          offerData.map((data, index) => (
-            <div key={index} className='shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400 hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default'>
-              {data.icon}
-              <h1 className='font-semibold text-md md:text-lg'>
-                {data.title}
-              </h1>
-
-              <p className='text-sm md:text-base'>
-                {data.description}
-              </p>
-            </div>
-          ))
-        }    
+      {/* 🔹 Offer Section */}
+      <div className="grid grid-col gap-5 my-5 border-gray-400 border-b-4 pb-10 md:grid-cols-2">
+        {offerData.map((data, index) => (
+          <div
+            key={index}
+            className="shadow-md shadow-gray-300 p-2 rounded-xl transition-all duration-400 hover:-translate-y-3 hover:bg-[#1E293B] hover:text-white cursor-default"
+          >
+            {data.icon}
+            <h1 className="font-semibold text-md md:text-lg">{data.title}</h1>
+            <p className="text-sm md:text-base">{data.description}</p>
+          </div>
+        ))}
       </div>
 
-      <div className='my-10'>
-        <h1 className='flex items-center text-2xl font-semibold my-4'>My Small Projects</h1>
-        <div className='grid grid-cols-1 gap-5 md:grid-cols-2'>
-          <Projects 
-            title={"My Portfolio"} 
-            Icon={UserCircleIcon}
-            description={"A personal Portfolio made using basic HTML, CSS, and a bit of JavaScript."} 
-            image={Portfolio1}
-            alt={"Portfolio"}
-            titleColor={"text-blue-400"}
-            onClick={portfolio1}
-          />
-
-          <Projects 
-            title={"Random Jokes"} 
-            Icon={Laugh}
-            description={"A simple school output build with HTML, CSS, and JavaScript that tells random jokes."} 
-            image={RandomJokes}
-            alt={"Random Jokes"}
-            titleColor={"text-[#8DA4D0]"}
-            onClick={randomJokes}
-          />
-
-          <Projects 
-            title={"FitXpert"} 
-            Icon={Dumbbell}
-            description={"A basic school project created using HTML, CSS, and JavaScript. It's a gym website that displays pricing and allows users to apply for the program, and similar features."} 
-            image={FitXpert}
-            alt={"FitXpert"}
-            titleColor={"text-red-400"}
-            onClick={fitExpert}
-          />
-
-          <Projects 
-            title={"Intrams Survey"} 
-            Icon={ScrollText}
-            description={"A school project that collects student feedback on intramurals, including favorite events and suggestions, and displays the results in a simple interface."} 
-            image={IntramsSurvey}
-            alt={"Intrams Survey"}
-            titleColor={"text-sky-300"}
-            onClick={intramsSurvey}
-          />
-
-          <Projects 
-            title={"My Portfolio"} 
-            Icon={GraduationCap}
-            description={"My second personal portfolio, built with ReactJS and TailwindCSS. It showcases my improved design approach, though it is not yet fully responsive."} 
-            image={Portfolio2}
-            alt={"Portfolio2"}
-            titleColor={"text-white"}
-            onClick={portfolio2}
-          />
-
+      {/* 🔹 Projects Section */}
+      <div className="my-10">
+        <h1 className="flex items-center text-2xl font-semibold my-4">
+          My Small Projects
+        </h1>
+        <div className="grid grid-cols-1 gap-5 md:grid-cols-2">
+          {projectsData.map((proj, i) => (
+            <Projects
+              key={i}
+              title={proj.title}
+              Icon={proj.Icon}
+              description={proj.description}
+              image={proj.image}
+              alt={proj.alt}
+              titleColor={proj.titleColor}
+              onClick={() => window.open(proj.link, "_blank")}
+            />
+          ))}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default ServicesProjects
+export default ServicesProjects;
